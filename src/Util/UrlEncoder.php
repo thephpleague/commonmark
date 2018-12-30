e959590a21d06b675beb7f45fac18673407efe49
Colin O'Dell <colinodell@gmail.com>
2018-12-29 20:39:28 -0500

    Fix XSS vulnerability caused by URL normalization not encoding newlines

--- src/Util/UrlEncoder.php
+++ src/Util/UrlEncoder.php
@@ -82,7 +82,7 @@ final class UrlEncoder
      */
     private static function encode($uri)
     {
-        return preg_replace_callback('/(%[0-9a-f]{2})|./iu', function ($matches) {
+        return preg_replace_callback('/(%[0-9a-f]{2})|./isu', function ($matches) {
             // Keep already-encoded characters as-is
             if (count($matches) > 1) {
                 return $matches[0];
