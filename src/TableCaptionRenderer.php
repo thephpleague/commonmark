bf865993aaaf16e389f3de28d9e6d9235f7cb13a
nanaya <me@nanaya.pro>
2019-09-25 19:02:06 +0900

    Remove manual escaping of attributes

--- src/TableCaptionRenderer.php
+++ src/TableCaptionRenderer.php
@@ -19,7 +19,6 @@ use League\CommonMark\Block\Element\AbstractBlock;
 use League\CommonMark\Block\Renderer\BlockRendererInterface;
 use League\CommonMark\ElementRendererInterface;
 use League\CommonMark\HtmlElement;
-use League\CommonMark\Util\Xml;
 
 final class TableCaptionRenderer implements BlockRendererInterface
 {
@@ -29,10 +28,7 @@ final class TableCaptionRenderer implements BlockRendererInterface
             throw new \InvalidArgumentException('Incompatible block type: '.get_class($block));
         }
 
-        $attrs = [];
-        foreach ($block->getData('attributes', []) as $key => $value) {
-            $attrs[$key] = Xml::escape($value);
-        }
+        $attrs = $block->getData('attributes', []);
 
         if ($block->id) {
             $attrs['id'] = $block->id;
