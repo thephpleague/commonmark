a946e3e69550fd9dd297e9bb4a9559a68d746033
Colin O'Dell <colinodell@gmail.com>
2019-10-31 09:11:40 -0400

    Add missing deprecation notice

--- src/Util/Html5Entities.php
+++ src/Util/Html5Entities.php
@@ -14,6 +14,8 @@
 
 namespace League\CommonMark\Util;
 
+@trigger_error(sprintf('The "%s" class is deprecated since league/commonmark 1.1, use "%s" instead.', Html5Entities::class, Html5EntityDecoder::class), E_USER_DEPRECATED);
+
 /**
  * @deprecated Use Html5EntityDecoder instead
  */
