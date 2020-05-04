412639f7cfbc0b31ad2455b2fe965095f66ae505
Colin O'Dell <colinodell@gmail.com>
2020-05-04 18:15:21 -0400

    Release 1.4.3

--- src/CommonMarkConverter.php
+++ src/CommonMarkConverter.php
@@ -24,7 +24,7 @@ class CommonMarkConverter extends Converter
      *
      * This might be a typical `x.y.z` version, or `x.y-dev`.
      */
-    public const VERSION = '1.4-dev';
+    public const VERSION = '1.4.3';
 
     /** @var EnvironmentInterface */
     protected $environment;
