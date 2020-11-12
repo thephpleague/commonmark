d55aec7772d9d8231f75827288b56994da58cb9b
Colin O'Dell <colinodell@gmail.com>
2020-11-12 10:58:53 -0500

    Deprecate passing an Environment into the specialized Converter constructors

--- src/GithubFlavoredMarkdownConverter.php
+++ src/GithubFlavoredMarkdownConverter.php
@@ -26,17 +26,20 @@ final class GithubFlavoredMarkdownConverter extends MarkdownConverter
     /**
      * Create a new commonmark converter instance.
      *
-     * @param array<string, mixed> $config
+     * @param array<string, mixed>      $config
+     * @param EnvironmentInterface|null $environment DEPRECATED - Instantiate a MarkdownConverter instead
      */
     public function __construct(array $config = [], ?EnvironmentInterface $environment = null)
     {
         // Passing in an $environment is deprecated
         if ($environment !== null) {
+            @\trigger_error('Passing an $environment into the GithubFlavoredMarkdownConverter constructor is deprecated in league/commonmark v2.0 and will be removed in v3.0; use MarkdownConverter instead of CommonMarkConverter', \E_USER_DEPRECATED);
             if ($config !== []) {
                 if (! ($environment instanceof Environment)) {
                     throw new \RuntimeException('Unable to configure the environment as only ' . Environment::class . ' can be configured after instantiation');
                 }
 
+                @\trigger_error('Configuring custom environments via the constructor is deprecated in league/commonmark v2.0 and will be removed in v3.0; configure it beforehand and create MarkdownConverter with it instead', \E_USER_DEPRECATED);
                 $environment->mergeConfig($config);
             }
 
