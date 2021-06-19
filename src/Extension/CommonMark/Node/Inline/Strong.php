8a932f6450f5d6c62209bc0a5e031e637dae5a5c
Colin O'Dell <colinodell@gmail.com>
2021-06-19 14:15:57 -0400

    Fix typo

--- src/Extension/CommonMark/Node/Inline/Strong.php
+++ src/Extension/CommonMark/Node/Inline/Strong.php
@@ -21,22 +21,22 @@ use League\CommonMark\Node\Inline\DelimitedInterface;
 
 final class Strong extends AbstractInline implements DelimitedInterface
 {
-    private string $delimeter;
+    private string $delimiter;
 
-    public function __construct(string $delimeter = '**')
+    public function __construct(string $delimiter = '**')
     {
         parent::__construct();
 
-        $this->delimeter = $delimeter;
+        $this->delimiter = $delimiter;
     }
 
     public function getOpeningDelimiter(): string
     {
-        return $this->delimeter;
+        return $this->delimiter;
     }
 
     public function getClosingDelimiter(): string
     {
-        return $this->delimeter;
+        return $this->delimiter;
     }
 }
